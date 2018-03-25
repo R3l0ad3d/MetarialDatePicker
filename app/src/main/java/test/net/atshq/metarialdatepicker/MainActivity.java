bbc500f6d08ce753acb1aa21d43d5ac2e0c8d899
Joy <azizur.rahman.bu@gmail.com>
2018-03-25 15:53:41 +0600

    time picker add

--- app/src/main/java/test/net/atshq/metarialdatepicker/MainActivity.java
+++ app/src/main/java/test/net/atshq/metarialdatepicker/MainActivity.java
@@ -1,31 +1,98 @@
 package test.net.atshq.metarialdatepicker;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.Timepoint;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     private TextView tvDate;
-
+    private TextView tvTime;
     Calendar myCalendar;
     String dateSt="";
+
+    private TimePickerDialog tpd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvDate = findViewById(R.id.tvDateShow);
+        tvTime = findViewById(R.id.tvTimeShow);
     }
+
+    void timePicker(){
+        Calendar now = Calendar.getInstance();
+                    /*
+                    It is recommended to always create a new instance whenever you need to show a Dialog.
+                    The sample app is reusing them because it is useful when looking for regressions
+                    during testing
+                     */
+        if (tpd == null) {
+            tpd = TimePickerDialog.newInstance(
+                    this,
+                    now.get(Calendar.HOUR_OF_DAY),
+                    now.get(Calendar.MINUTE),
+                   true
+            );
+        } else {
+            tpd.initialize(
+                    this,
+                    now.get(Calendar.HOUR_OF_DAY),
+                    now.get(Calendar.MINUTE),
+                    now.get(Calendar.SECOND),
+                    true);
+        }
+        tpd.setThemeDark(true);
+        tpd.vibrate(true);
+        tpd.dismissOnPause(true);
+        tpd.enableSeconds(false);
+        tpd.setVersion(false ? TimePickerDialog.Version.VERSION_2 : TimePickerDialog.Version.VERSION_1);
+        if (false) {
+            tpd.setAccentColor(Color.parseColor("#9C27B0"));
+        }
+        if (true) {
+            tpd.setTitle("TimePicker Title");
+        }
+        /*if (true) {
+            if (true) {
+                tpd.setTimeInterval(3, 5, 10);
+            } else {
+                tpd.setTimeInterval(3, 5, 60);
+            }
+        }
+        if (true) {
+            Timepoint[] disabledTimes = {
+                    new Timepoint(10),
+                    new Timepoint(10, 30),
+                    new Timepoint(11),
+                    new Timepoint(12, 30)
+            };
+            tpd.setDisabledTimes(disabledTimes);
+        }*/
+        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                Log.d("TimePicker", "Dialog was cancelled");
+            }
+        });
+        tpd.show(getSupportFragmentManager(), "Timepickerdialog");
+    }
+
 
 
     void datePiceker(){
@@ -84,5 +151,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void datePickClick(View view) {
         datePiceker();
+    }
+
+    public void timePickClick(View view) {
+        timePicker();
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+        String minuteString = minute < 10 ? "0"+minute : ""+minute;
+        String secondString = second < 10 ? "0"+second : ""+second;
+        String time = "You picked the following time: "+hourString+"h"+minuteString+"m"+secondString+"s";
+        tvTime.setText(time);
     }
 }
